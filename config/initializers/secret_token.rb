f421084059208d483d3b55ea225d352454f88bab
Eli Anderson <elianderson03@gmail.com>
2011-01-20 18:37:08 -0800

    Inciated base rails proj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wwyrb::Application.config.secret_token = '89993c25eb135877d5a2834a708e594de4d2a178c6938c39c75c05f5b925396aa96ac971bf466b77ed30a1db78a22183885e321238a422be236e2a6de1312277'
